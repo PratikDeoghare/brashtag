1d41641ac4a002fab7fbb26fec93eb7c50a84129
Pratik Deoghare <pratik.deoghare@gmail.com>
2021-12-28 11:04:55 +0100

    WIP error handling

--- parser_test.go
+++ parser_test.go
@@ -1,8 +1,9 @@
 package brashtag_test
 
 import (
+	"os"
 	"testing"
-
+	
 	bt "github.com/pratikdeoghare/brashtag"
 )
 
@@ -11,15 +12,32 @@ func TestParser(t *testing.T) {
 	a := bt.NewCode("$$", "echo $PATH")
 	b := bt.NewBlob("some text")
 	r.AddKids(a, b)
-
+	
 	r2, err := bt.Parse(r.String())
 	if err != nil {
 		t.Fatal(err)
 	}
-
+	
 	v := r2.(bt.Bag).Kids()[0]
-
+	
 	if v.String() != r.String() {
 		t.Fail()
 	}
+}
+
+func TestUnexpectedL(t *testing.T) {
+	data, err := os.ReadFile("./testdata/fail1.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = bt.Parse(string(data))
+	if err == nil {
+		t.Fatal("expected error")
+	}
+	t.Log(err)
+}
+
+func TestFail2(t *testing.T) {
+	// TODO
+	t.Fail()
 }
