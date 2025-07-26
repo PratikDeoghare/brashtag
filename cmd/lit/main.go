0cbc1d81f16f56ca80f5746771939580c61c2d81
Pratik Deoghare <pratik.deoghare@gmail.com>
2025-07-26 16:28:16 +0200

    refactor lit (#8)

--- cmd/lit/main.go
+++ cmd/lit/main.go
@@ -4,8 +4,8 @@ import (
 	"flag"
 	"fmt"
 	bt "github.com/pratikdeoghare/brashtag"
+	"github.com/pratikdeoghare/brashtag/cmd/lit/extractor"
 	"os"
-	"regexp"
 	"strings"
 )
 
@@ -30,13 +30,8 @@ func weave(filename, outFile string) {
 		panic(err)
 	}
 
-	a := &A{
-		tree: tree,
-		m:    make(map[string]string),
-		deps: make(map[string][]dep),
-	}
-	a.buildDeps(a.tree, "")
-	fmt.Println(a.printProg(fmt.Sprintf("<<<%s>>>", outFile)))
+	a := extractor.NewExtractor(tree)
+	fmt.Println(a.PrintProg(fmt.Sprintf("<<<%s>>>", outFile)))
 }
 
 func tangle(filename string) {
@@ -51,108 +46,6 @@ func tangle(filename string) {
 		panic(err)
 	}
 
-	a := &A{
-		tree: tree,
-		m:    make(map[string]string),
-		deps: make(map[string][]dep),
-	}
-	a.buildDeps(a.tree, "")
-	fmt.Println(a.printProg("<<<main.go>>>"))
-}
-
-func (a A) printProg(root string) string {
-	s := a.m[root]
-
-	var lines []string
-	for _, line := range strings.Split(s, "\n") {
-		if spaceThunk.Match([]byte(line)) {
-			idx := strings.Index(line, "<")
-			prefix := line[:idx]
-			for _, thunkLine := range strings.Split(a.printProg(strings.TrimSpace(line)), "\n") {
-				lines = append(lines, prefix+thunkLine)
-			}
-		} else {
-			lines = append(lines, line)
-		}
-	}
-
-	return strings.Join(lines, "\n")
-}
-
-type A struct {
-	tree bt.Node
-	m    map[string]string
-	deps map[string][]dep
-}
-
-type dep struct {
-	id     string
-	prefix string
-}
-
-func (a *A) buildDeps(tree bt.Node, path string) {
-	path = fmt.Sprintf("<<<%s>>>", path)
-	switch x := tree.(type) {
-	case bt.Blob:
-
-	case bt.Code:
-		a.m[path] = stripMargin(x.Text())
-		a.deps[path] = extractDeps(x.Text())
-
-	case bt.Bag:
-		for _, k := range x.Kids() {
-			a.buildDeps(k, x.Tag())
-		}
-	}
-
-}
-
-func stripMargin(s string) string {
-	lines := strings.Split(s, "\n")
-	i := 0
-	if len(lines) >= 2 {
-		for i < len(lines[1]) && lines[1][i] == ' ' {
-			i++
-		}
-	} else {
-		return s
-	}
-	for j, line := range lines {
-		if len(line) >= i && strings.TrimSpace(line[:i]) == "" {
-			lines[j] = line[i:]
-		} else {
-			lines[j] = line
-		}
-	}
-	return strings.Join(lines, "\n")
-}
-
-func stripMargin1(s string) string {
-	var lines []string
-	for _, line := range strings.Split(s, "\n") {
-		line2 := line
-		line = strings.TrimSpace(line)
-		if len(line) >= 2 && line[0] == '|' {
-			line = line[2:]
-		} else {
-			line = line2
-		}
-		lines = append(lines, line)
-	}
-	x := strings.Join(lines, "\n")
-	return x
-}
-
-var thunk = regexp.MustCompile(`<<<.*>>>`)
-var spaceThunk = regexp.MustCompile(`^\s*<<<.*>>>`)
-
-func extractDeps(text string) []dep {
-	var deps []dep
-	for _, line := range strings.Split(text, "\n") {
-		if thunk.Match([]byte(line)) {
-			idx := strings.Index(line, "<")
-			deps = append(deps, dep{id: strings.TrimSpace(line[idx:]), prefix: line[:idx]})
-		}
-	}
-	return deps
+	a := extractor.NewExtractor(tree)
+	fmt.Println(a.PrintProg("<<<main.go>>>"))
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
 module github.com/pratikdeoghare/brashtag
 
-go 1.16
+go 1.23
 
-require github.com/goccy/go-graphviz v0.1.1
+require (
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d
+	github.com/goccy/go-graphviz v0.1.1
+	golang.org/x/net v0.8.0
+)
