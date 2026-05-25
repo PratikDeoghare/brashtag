1a1dfc8c9f4b9d163c226890a7a38589bbe7c537
Pratik Deoghare <pratik.deoghare@gmail.com>
2026-05-25 17:41:28 +0200

    add python package

---
+++ brashtag-py/brashtag/__init__.py
@@ -0,0 +1,9 @@
+from .parser import (
+    Bag, Blob, Code,
+    parse, parse_bag, parse_blob, parse_code
+)
+
+__all__ = [
+    "Bag", "Blob", "Code",
+    "parse", "parse_bag", "parse_blob", "parse_code"
+]
